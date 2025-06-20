5535788932a4915c00f0ba1ab771be90f9755c4d
sim3rat <aswaniak0322@gmail.com>
2025-06-19 22:58:11 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+body {
+  font-family: 'Segoe UI', sans-serif;
+  margin: 0;
+  padding: 0;
+}
+
+header {
+  background-color: #111;
+  color: white;
+  padding: 20px;
+  text-align: center;
+}
+
+nav a {
+  color: white;
+  margin: 0 10px;
+  text-decoration: none;
+}
+
+#combos {
+  padding: 20px;
+}
+
+.combo-card {
+  border: 1px solid #ccc;
+  padding: 15px;
+  max-width: 300px;
+  margin: 20px auto;
+  text-align: center;
+}
+
+.combo-card img {
+  width: 100%;
+  height: auto;
+}
